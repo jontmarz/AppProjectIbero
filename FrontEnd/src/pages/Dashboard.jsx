e819a315c1064ec70cad28a7046c1f22ce11191c
John Edwin Torres Martínez <34428004+jontmarz@users.noreply.github.com>
2023-10-10 10:47:37 -0500

    modulo docente backend

--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -1,34 +1,10 @@
 import { useState } from 'react'
-import { api } from "../config/axios";
-import { getToken, deleteToken } from "../config/axios";
-import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
 
 export default function Dashboard() {
-    const [user, setUser] = useState([])
-    const token = getToken()
-    const navigate = useNavigate()
-    
-    const getUser = async() => {
-
-        try {
-            const { data } = await api({
-                url: "/api/user/info",
-                method: "GET",
-            })
-            setUser(data.infoUser)
-            console.log(data);
-        } catch (e) {
-            console.error('error: ' + e.response);
-            /* if (e.response.status >= 500 || e.response.status >= 400) {
-                deleteToken()
-                setUser(false)
-                navigate("/")
-            } */
-        }
-    }
-    getUser()
+    const { user, setUser } = useUserContext();
 
   return (
-    <div>Usuario: {user}</div>
+    <div>Usuario: {user.fullName}</div>
   )
 }
